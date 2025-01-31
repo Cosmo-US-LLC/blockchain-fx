2158008c1d20bdc15f7bafd5c86acc95cf89c80b
Saif <saifali@cosmoinc.co>
2025-01-31 17:04:09 +0500

    BFX resized

--- src/Home/HyperGrowthSec.jsx
+++ src/Home/HyperGrowthSec.jsx
@@ -3,45 +3,57 @@ import React from "react";
 import graph from "../assets/HyperGrowth/Graph.png";
 
 function HyperGrowthSec() {
- 
   return (
-    <div className="pt-[61px] space-y-[35px] pb-[66px] bg-[#FAFAFA]">
-      <div className="max-w-[919px] space-y-[60px] w-[100%] mx-auto ">
-        <div className="space-y-[30px]">
-          <h3 className="text-[50px] font-[700] text-center leading-[114%] text-[#181A20] tracking-[-1px]">
-          Hyper Growth Model
+    <div className="py-[50px] bg-[#FAFAFA]">
+      <div className="max-w-[1200px] space-y-[35px] w-[100%] mx-auto ">
+        <div className="space-y-[14px]">
+          <h3 className="text-[45px] font-[700] text-center leading-[114%] text-[#181A20] tracking-[-1px]">
+            Hyper Growth Model
           </h3>
-          <p className="text-[20px] font-[400] max-w-[832px] mx-auto w-[100%] text-center leading-[130%] text-[#181A20] tracking-[-0.32px]">
-          BlockchainFX is the first community-driven trading platform that rewards users through a staking revenue model, building a loyal and active community. As the platform grows, so does the token, and vice versa, creating a cycle of continuous rewards and engagement. Studies show that incentivized communities grow up to 10 times faster than others, and BFX is designed to maximize that potential.
+          <p className="text-[14px] font-[400] max-w-[908px] mx-auto w-[100%] text-center leading-[21px] text-[#181A20] tracking-[-0.32px]">
+            BlockchainFX is the first community-driven trading platform with a
+            staking revenue redistribution model that continuously rewards users
+            and its community. This drives rapid organic growth through network
+            effects. As the platform grows, so will the token, and vice versa.
+            Studies show that incentivized communities grow up to 10 times
+            faster on average than others.
           </p>
         </div>
-        <div className="space-y-[70px] bg-[#fff] rounded-[14px] p-[70.32px]" 
-        style={{
-            boxShadow:"0px 7.032px 35.16px 0px rgba(169, 169, 169, 0.30)"
-        }}
+        <div
+          className="space-y-[53px] bg-[#fff] rounded-[14px] p-[53.32px] w-[667px] h-[413px] mx-auto"
+          style={{
+            boxShadow: "0px 5.336px 26.68px 0px rgba(169, 169, 169, 0.30)",
+            // box-shadow: 0px 5.336px 26.68px 0px rgba(169, 169, 169, 0.30);
+          }}
         >
           <div className="flex items-center justify-between">
-            <h4 className="text-[#000] font-[500] text-[26px]">Growth Forecasting</h4>
+            <h4 className="text-[#000] font-[500] text-[19.7px]">
+              Growth Forecasting
+            </h4>
             <div className="flex space-x-3 items-center">
-                <hr className="h-[5px] w-[20px] bg-[#E5AE00]"/>
-                <p className="text-[18px] text-[#949494] font-[500]">BlockchainFX</p>
+              <hr className="h-[0px] w-[20px] border-[2px] border-[#E5AE00]" />
+              <p className="text-[13.6px] text-[#949494] font-[500]">
+                BlockchainFX
+              </p>
             </div>
             <div className="flex space-x-3 items-center">
-                <hr className="h-[5px] w-[20px] bg-[#CE2A96]"/>
-                <p className="text-[18px] text-[#949494] font-[500]">Regular Trading Platform</p>
+              <hr className="h-[0px] w-[20px] border-[2px] border-[#CE2A96]" />
+              <p className="text-[13.6px] text-[#949494] font-[500]">
+                Regular Trading Platform
+              </p>
             </div>
           </div>
           <div>
-            <img src={graph} alt="" />
+            <img src={graph} alt="" className="" />
           </div>
-         
         </div>
         <div className="flex justify-center items-center">
-        <button 
-        onClick={() => window.scrollTo({ top: 0, behavior: 'smooth'})}
-        className="text-white bg-[#E5AE00] px-[12px] hover:text-black hover:bg-transparent text-[18px] font-[800] border border-[#E5AE00] hover:border-[#000] rounded-[8px] max-w-[152px] w-[100%] h-[58px]">
-              Buy $BFX
-            </button>
+          <button
+            onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
+            className="text-white bg-[#E5AE00] px-[12px] hover:text-black hover:bg-transparent text-[14px] font-[800] border border-[#E5AE00] hover:border-[#000] rounded-[8px] max-w-[152px] w-[100%] h-[39px]"
+          >
+            Buy $BFX
+          </button>
         </div>
       </div>
     </div>
--- src/Home/LessThanSec.jsx
+++ src/Home/LessThanSec.jsx
@@ -3,71 +3,73 @@ import React from "react";
 import graph from "../assets/LessThanSec/Frame 1410125779 (1).png";
 
 function LessThanSec() {
- 
   return (
-    <div className="pt-[98px] space-y-[35px] pb-[92px] bg-[#e8e8e885]">
-      <div className="max-w-[1220px] space-y-[60px] w-[100%] mx-auto ">
-        <div className="space-y-[30px]">
-          <h3 className="text-[50px] capitalize font-[700] text-center leading-[114%] text-[#181A20] tracking-[-1px]">
-          The Bridge Between <br />
-          Blockchain and Global Finance
+    <div className="pt-[50px] pb-[50px] bg-[#fff]">
+      <div className="max-w-[1200px] space-y-[50px] w-[100%] mx-auto ">
+        <div className="space-y-[18px]">
+          <h3 className="text-[45px] capitalize font-[700] text-center leading-[58px] text-[#181A20] tracking-[-1px]">
+            The Bridge Between <br />
+            Blockchain and Global Finance
           </h3>
-          <p className="text-[20px] font-[400] max-w-[832px] mx-auto w-[100%] text-center leading-[130%] text-[#181A20] tracking-[-0.32px]">
-          Crypto makes up only 0.87% of global trading volume in 2024, averaging $84.3 billion daily. Meanwhile, the Forex market alone handles $7.5 trillion per day. This gap presents a major opportunity for $BFX to connect crypto users with the world’s largest financial markets.
+          <p className="text-[14px] font-[400] w-[832px] mx-auto text-center leading-[130%] text-[#181A20] tracking-[-0.32px]">
+            Crypto makes up only 0.87% of global trading volume in 2024,
+            averaging $84.3 billion daily, while the Forex market alone handles
+            $7.5 trillion daily. This creates an opportunity for $BFX to bridge
+            crypto users with the world’s largest markets.
           </p>
         </div>
-        <div className="space-y-[70px]">
-          <h4 className="text-[20px] space-y-[70px] font-[700] text-center leading-[130%] text-[#181A20] tracking-[-1px]">
+        <div className="space-y-[60px]">
+          {/* <h4 className="text-[20px] space-y-[50px] font-[700] text-center leading-[130%] text-[#181A20] tracking-[-1px]">
             Average Daily Trading Volume in 2024
-          </h4>
+          </h4> */}
           <div className="relative">
             <div className="flex justify-center">
-              <img src={graph} alt="" />
+              <img src={graph} alt="" className="h-[321px] w-[321px]" />
             </div>
-            <p className="text-[#6F6F6F] top-0 left-[18%] absolute text-[20px] font-[700] tracking-[-0.787px]">
-              Bonds: $1.1 trillion (11.42%)
+            <p className="text-[#6F6F6F] -top-3 left-[28%] absolute text-[15px] font-[700] tracking-[-0.787px]">
+              Bonds: $1.1 Trillion (11.42%)
             </p>
-            <p className="text-[#6F6F6F] absolute top-[12%] left-[10%] text-[20px] font-[700] tracking-[-0.787px]">
-              U.S. Stocks: $700 billion (7.27%)
+            <p className="text-[#6F6F6F] absolute top-[8%] left-[20%] text-[15px] font-[700] tracking-[-0.787px]">
+              U.S. Stocks: $700 Billion (7.27%)
             </p>
-            <p className="text-[#6F6F6F] absolute top-[24%] left-[6%] text-[20px] font-[700] tracking-[-0.787px]">
-              Commodities: $250 billion (2.59%)
+            <p className="text-[#6F6F6F] absolute top-[21%] left-[17%] text-[15px] font-[700] tracking-[-0.787px]">
+              Commodities: $250 Billion (2.59%)
             </p>
-            <p className="text-[#6F6F6F] absolute top-[35%] left-[10%]  text-[20px] font-[700] tracking-[-0.787px]">
+            <p className="text-[#6F6F6F] absolute top-[35%] left-[19%]  text-[15px] font-[700] tracking-[-0.787px]">
               Crypto: $89 Billion (0.87%)
             </p>
-            <p className="text-[#6F6F6F] absolute bottom-[7%] right-[13%]  text-[20px] font-[700] tracking-[-0.787px]">
-              Forex: $7.5 trillion (77.85%)
+            <p className="text-[#6F6F6F] absolute bottom-[5%] right-[23%]  text-[15px] font-[700] tracking-[-0.787px]">
+              Forex: $7.5 Trillion (77.85%)
             </p>
           </div>
-          <div className="max-w-[887px] pt-[80px] flex justify-between items-center mx-auto w-[100%]">
+          <div className="max-w-[887px] pt-[0px] flex justify-between items-center mx-auto w-[100%]">
             <div className="flex items-center space-x-2">
-              <div className="w-[36px] h-[36px] bg-[#E9AD2F] rounded-[4px]"></div>
-              <p className="text-[#181A20] text-[20px] font-[700] tracking-[-1px] ">
+              <div className="w-[30px] h-[30px] bg-[#E9AD2F] rounded-[4px]"></div>
+              <p className="text-[#181A20] text-[16.7px] font-[700] tracking-[-1px] ">
                 Forex
               </p>
             </div>
             <div className="flex items-center space-x-2">
-              <div className="w-[36px] h-[36px] bg-[#DCDCDC] rounded-[4px]"></div>
-              <p className="text-[#181A20] text-[20px] font-[700] tracking-[-1px] ">
+              <div className="w-[30px] h-[30px] bg-[#DCDCDC] rounded-[4px]"></div>
+              <p className="text-[#181A20] text-[16.7px] font-[700] tracking-[-1px] ">
                 Bonds
               </p>
             </div>
             <div className="flex items-center space-x-2">
-              <div className="w-[36px] h-[36px] bg-[#B6B6B6] rounded-[4px]"></div>
-              <p className="text-[#181A20] text-[20px] font-[700] tracking-[-1px] ">
+              <div className="w-[30px] h-[30px] bg-[#B6B6B6] rounded-[4px]"></div>
+              <p className="text-[#181A20] text-[16.7px] font-[700] tracking-[-1px] ">
                 US Stock
               </p>
             </div>
             <div className="flex items-center space-x-2">
-              <div className="w-[36px] h-[36px] bg-[#7DBB6C] rounded-[4px]"></div>
-              <p className="text-[#181A20] text-[20px] font-[700] tracking-[-1px] ">
+              <div className="w-[30px] h-[30px] bg-[#7DBB6C] rounded-[4px]"></div>
+              <p className="text-[#181A20] text-[16.7px] font-[700] tracking-[-1px] ">
                 Commodities
               </p>
             </div>
             <div className="flex items-center space-x-2">
-              <div className="w-[36px] h-[36px] bg-[#A36E00] rounded-[4px]"></div>
-              <p className="text-[#181A20] text-[20px] font-[700] tracking-[-1px] ">
+              <div className="w-[30px] h-[30px] bg-[#A36E00] rounded-[4px]"></div>
+              <p className="text-[#181A20] text-[16.7px] font-[700] tracking-[-1px] ">
                 Crypto
               </p>
             </div>
