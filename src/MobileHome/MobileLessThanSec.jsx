9128a5c99eb31862241c123c2b9ce075f202bdb8
faheem303 <faheemrehmat360@gmail.com>
2025-04-11 19:46:45 +0500

    mobile Carosuel

--- src/MobileHome/MobileLessThanSec.jsx
+++ src/MobileHome/MobileLessThanSec.jsx
@@ -5,6 +5,7 @@ import { Navigation, Pagination } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
 import graph from "../assets/LessThanSec/Frame 1410125779 (1).png";
 import ellipse from "../assets/EarnOnSec/Ellipse 3.png";
+import ellipse2 from "../assets/EarnOnSec/Ellipse 4.png";
 
 import graphyp from "../assets/HyperGrowth/Graph.png";
 import icon1 from "../assets/Gateway/gticons (4).svg";
@@ -43,7 +44,7 @@ function MobileLessThanSec() {
     };
   return (
     <div className="pt-[24px] pb-[24px] bg-[#030F16] overflow-hidden">
-        <div className="space-y-[32px] py-4 px-2 w-[90%] mx-auto border-[1px] border-[#2B2B2B] relative overflow-hidden">
+        {/* <div className="space-y-[32px] py-4 px-2 w-[90%] mx-auto border-[1px] border-[#2B2B2B] relative overflow-hidden">
         <img
           src={ellipse}
           className="max-w-[400px] h-[450px]  absolute top-[0%] left-[0%]  "
@@ -119,17 +120,9 @@ function MobileLessThanSec() {
             </div>
           </div>
         </div>
-      </div>
+      </div> */}
      
-        {/* <div className="relative max-w-[1200px] mx-auto">
-                <div className="w-[40%] flex justify-end items-end space-x-2">
-                          <button className="swiper-button-prev-swp a absolute z-[99] -left-0 top-[50%] transform -translate-y-1/2">
-                            <img className="w-[42.545px] h-[42.545px]" src={arw} alt="" />
-                          </button>
-                          <button className="swiper-button-next-swp -right-0 top-[50%] absolute z-[99] rotate-[182deg]  transform -translate-y-1/2">
-                            <img className="w-[42.545px] h-[42.545px]" src={arw} alt="" />
-                          </button>
-                        </div>
+        <div className="relative max-w-[1200px] mx-auto">
                   <Swiper
                     slidesPerView={1}
                     spaceBetween={20}
@@ -152,10 +145,15 @@ function MobileLessThanSec() {
                     className="mySwiper"
                   >
                       <SwiperSlide>
-                      <div className="space-y-[32px] py-4 px-2 w-[90%] mx-auto border-[1px] border-[#2B2B2B] relative overflow-hidden">
+                      <div className="space-y-[32px] min-h-[1234px] py-4 px-2 w-[90%] mx-auto border-[1px] border-[#2B2B2B] relative overflow-hidden rounded-[16px]">
         <img
           src={ellipse}
           className="max-w-[400px] h-[450px]  absolute top-[0%] left-[0%]  "
+          alt=""
+        />
+         <img
+          src={ellipse2}
+          className="max-w-[400px] h-[450px]  absolute bottom-[0%] right-[0%]  "
           alt=""
         />
 
@@ -163,16 +161,14 @@ function MobileLessThanSec() {
           <h3 className="text-[30px] capitalize font-[600] text-center leading-[120%] text-[#FFF] tracking-[-2.548px]">
             Bridging Crypto and Finance
           </h3>
-          <p className="text-[15px] font-[400]  w-[100%] text-center leading-[175%] text-[#8497A2] tracking-[-0.32px]">
-            Crypto makes up only 0.87% of global trading volume in 2024,
-            averaging $84.3 billion daily. Meanwhile, the Forex market alone
-            handles $7.5 trillion per day. This gap presents a major opportunity
-            for $BFX to connect crypto users with the world’s largest financial
-            markets.
+          <p className="text-[15px] font-[400]  w-[100%] text-center leading-[175%] text-[#fff] tracking-[-0.32px]">
+          Crypto makes up only 0.87% of global trading volume in 2024, averaging $84.3 billion daily. <br /><br />
+          Meanwhile, the Forex market alone handles  $7.5 trillion per day. This gap presents a major opportunity for $BFX to connect crypto users 
+with the world’s largest financial markets.
           </p>
         </div>
-        <div className="space-y-[70px]">
-          <h4 className="text-[15px] space-y-[70px] font-[600] text-center leading-[130%] text-[#6F6F6F] tracking-[-0.617px]">
+        <div className="space-y-[70px] py-[30px]">
+          <h4 className="text-[15px] space-y-[70px] font-[600] text-center leading-[130%] text-[#fff] tracking-[-0.617px]">
             Average Daily Trading Volume in 2024
           </h4>
           <div className="relative">
@@ -195,7 +191,7 @@ function MobileLessThanSec() {
               Forex: <br /> $7.5 trillion (77.85%)
             </p>
           </div>
-          <div className="grid grid-cols-2 gap-y-[1rem] pt-[20px] mx-auto w-[100%]">
+          <div className="grid grid-cols-2 gap-y-[3rem] pt-[20px] mx-auto w-[100%]">
             <div className="flex items-center space-x-2">
               <div className="w-[57px] h-[31px] bg-[#E9AD2F] rounded-[4px]"></div>
               <p className="text-[#8497A2] text-[15px] font-[600] tracking-[0.247px] ">
@@ -231,7 +227,7 @@ function MobileLessThanSec() {
       </div>
                       </SwiperSlide>
                     <SwiperSlide>
-                    <div className="relative space-y-[33px] rounded-[16px] py-[30px] px-[20px] overflow-hidden w-[90%] mx-auto "
+                    <div className="relative space-y-[33px] min-h-[1234px] rounded-[16px] py-[30px] px-[20px] overflow-hidden w-[90%] mx-auto "
       style={{
         border:"0.319px solid var(--stroke, rgba(230, 175, 3, 0.12))",
         background:"#030F16"
@@ -242,8 +238,13 @@ function MobileLessThanSec() {
                   className="absolute top-[0%] left-[0%]  "
                   alt=""
                 />
+                 <img
+          src={ellipse2}
+          className="max-w-[400px] h-[450px]  absolute bottom-[0%] right-[0%]  "
+          alt=""
+        />
         <div className="space-y-[20px] relative z-[9]">
-          <h3 className="text-[36px] font-[600] text-center leading-[120%] text-[#FFF] tracking-[-2.548px]">
+          <h3 className="text-[30px] font-[600] text-center leading-[120%] text-[#FFF] tracking-[-2.548px]">
           The Gateway to Global Markets
           </h3>
           <p className="text-[15px] font-[400] max-w-[881px] mx-auto w-[100%] text-center leading-[175%] text-[#8497A2] tracking-[-0.32px]">
@@ -387,13 +388,27 @@ function MobileLessThanSec() {
       </div>
                     </SwiperSlide>
                     <SwiperSlide>
-                    <div className="space-y-[30px] w-[90%] mx-auto ">
-        <div className="space-y-[20px]">
+                      <div className="space-y-[32px] min-h-[1234px] py-4 px-2 w-[90%] mx-auto border-[1px] border-[#2B2B2B] relative overflow-hidden rounded-[16px]">
+                      <img
+          src={ellipse}
+          className="max-w-[400px] h-[450px]  absolute top-[0%] left-[0%]  "
+          alt=""
+        />
+                   <img
+          src={ellipse2}
+          className="max-w-[400px] h-[450px]  absolute bottom-[0%] right-[0%]  "
+          alt=""
+        />
+
+                      <div className="space-y-[30px] mx-auto ">
+        <div className="space-y-[20px] pb-[30px]">
           <h3 className="text-[30px] font-[600] text-center leading-[120%] text-[#FFF] tracking-[-2.548px]">
           Designed For <br/> Exponential Growth
           </h3>
-          <p className="text-[15px] font-[400] max-w-[832px] mx-auto w-[100%] text-center leading-[130%] text-[#8497A2] tracking-[-0.32px]">
-          BlockchainFX is a community-driven trading platform that rewards users with daily staking rewards, fostering a loyal and active community. As the platform grows, so does the token, creating a cycle of rewards and engagement. Studies* show that incentivized communities can grow significantly faster than others, and BFX is designed to maximize that potential.
+          <p className="text-[15px]  font-[400] max-w-[832px] mx-auto w-[100%] text-center leading-[175%] text-[#8497A2] tracking-[-0.32px]">
+          BlockchainFX is a community-powered trading platform that rewards BFX holders daily. As the platform grows, so does the token — creating a powerful cycle of growth and engagement. Studies* show that incentivized communities can grow significantly 
+faster than others, and BFX is designed to 
+maximize that potential.
           </p>
         </div>
         <div
@@ -402,7 +417,7 @@ function MobileLessThanSec() {
             boxShadow: "0px 7.032px 35.16px 0px rgba(169, 169, 169, 0.30)",
           }}
         >
-          <div className="flex items-center justify-between">
+          <div className="flex items-center  justify-between">
             <h4 className="text-[#FFF] font-[500] text-[10.56px]">
               Growth Forecasting
             </h4>
@@ -419,12 +434,12 @@ function MobileLessThanSec() {
               </p>
             </div>
           </div>
-          <div>
+          <div className="py-[30px]">
             <img src={graphyp} alt="" />
           </div>
         </div>
         <div>
-          <p className="text-[14px] font-[400] max-w-[832px] mx-auto w-[100%] text-center leading-[130%] text-[#8497A2] tracking-[-0.32px]">(*Source: Harvard Business Review, When Community Becomes Your Competitive Advantage, 2020)</p>
+          <p className="text-[14px] py-[30px] font-[400] max-w-[832px] mx-auto w-[100%] text-center leading-[175%] text-[#8497A2] tracking-[-0.32px]">(*Source: Harvard Business Review, When Community Becomes Your Competitive Advantage, 2020)</p>
         </div>
         <div className="flex items-center justify-center ">
           <button
@@ -435,10 +450,21 @@ function MobileLessThanSec() {
           </button>
         </div>
       </div>
+                      </div>
+                   
                     </SwiperSlide>
                   </Swiper>
                   <div className="custom-pagination flex justify-center p-2 gap-3 mt-4"></div>
-                </div> */}
+                  <div className="pt-[30px] flex justify-center items-center space-x-2">
+                          <button className="swiper-button-prev-swp a  transform -translate-y-1/2">
+                            <img className="w-[42.545px] h-[42.545px]" src={arw} alt="" />
+                          </button>
+                          <button className="swiper-button-next-swp  rotate-[182deg]  transform -translate-y-1/2">
+                            <img className="w-[42.545px] h-[42.545px]" src={arw} alt="" />
+                          </button>
+                        </div>
+                </div>
+                
     </div>
   );
 }
