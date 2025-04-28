2e97293b41a7139a65fbf8a447eb86aef5e70af6
jawad <jawadakhtar@cosmoinc.co>
2025-04-28 17:04:40 +0500

    language pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React, { useState, useEffect } from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate, useParams, Outlet } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-
 import { Toaster } from "react-hot-toast";
 
 import Home from "./Home";
@@ -14,8 +13,43 @@ import TokenSale from "./TokenSale";
 import PrivacyPolicy from "./PrivacyPolicy";
 import CookieManagement from "./CookieManagement";
 import NavbarMobile from "./compunents/NavbarMobile";
-import LivePresale from "./compunents/LivePresale";
 import RefferalProgram from "./RefferalProgram";
+
+function Layout({ isMobile }) {
+  return (
+    <>
+      {isMobile ? <NavbarMobile /> : <Navbar />}
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
+    </>
+  );
+}
+
+function LangGuard({ children }) {
+  const { lang } = useParams();
+  const supportedLangs = [
+    "vi",
+    "de",
+    "nl",
+    "ja",
+    "tr",
+    "ko",
+    "it",
+    "no",
+    "zh",
+    "ru",
+    "fr",
+    "pt",
+    "es",
+    "ar",
+  ];
+  if (!supportedLangs.includes(lang)) {
+    return <Navigate to="/" replace />;
+  }
+  return children;
+}
 
 function App() {
   const [isMobile, setIsMobile] = useState(window.innerWidth < 700);
@@ -24,9 +58,7 @@ function App() {
     const handleResize = () => {
       setIsMobile(window.innerWidth < 700);
     };
-
     window.addEventListener("resize", handleResize);
-
     return () => {
       window.removeEventListener("resize", handleResize);
     };
@@ -36,16 +68,34 @@ function App() {
     <div className="bg-[#fff]">
       <ToastContainer />
       <Toaster position="bottom-center" />
-      {isMobile ? <NavbarMobile /> : <Navbar />}
       <Routes>
-        <Route path="/" element={isMobile ? <MobileHome /> : <Home />} />
-        <Route path="/terms-of-service" element={<TermsofService />} />
-        <Route path="/referral" element={<RefferalProgram />} />
-        <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-        <Route path="/token-sale" element={<TokenSale />} />
-        <Route path="/cookie-management" element={<CookieManagement />} />
+        {/* Default English routes */}
+        <Route element={<Layout isMobile={isMobile} />}>
+          <Route path="/" element={isMobile ? <MobileHome /> : <Home />} />
+          <Route path="/terms-of-service" element={<TermsofService />} />
+          <Route path="/referral" element={<RefferalProgram />} />
+          <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+          <Route path="/token-sale" element={<TokenSale />} />
+          <Route path="/cookie-management" element={<CookieManagement />} />
+        </Route>
+
+        {/* Other languages routes */}
+        <Route
+          path="/:lang"
+          element={
+            <LangGuard>
+              <Layout isMobile={isMobile} />
+            </LangGuard>
+          }
+        >
+          <Route index element={isMobile ? <MobileHome /> : <Home />} />
+          <Route path="terms-of-service" element={<TermsofService />} />
+          <Route path="referral" element={<RefferalProgram />} />
+          <Route path="privacy-policy" element={<PrivacyPolicy />} />
+          <Route path="token-sale" element={<TokenSale />} />
+          <Route path="cookie-management" element={<CookieManagement />} />
+        </Route>
       </Routes>
-      <Footer />
     </div>
   );
 }
