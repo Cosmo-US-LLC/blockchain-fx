07ad574ee19c11b745963dc488594c59f29bb643
faheem303 <faheemrehmat360@gmail.com>
2025-05-19 11:36:22 +0500

    issue icon

--- src/MobileHome/MobilePremiumSec.jsx
+++ src/MobileHome/MobilePremiumSec.jsx
@@ -484,9 +484,9 @@ function MobilePremiumSec() {
                             <div className="relative group inline-block">
                               <svg
                                 xmlns="http://www.w3.org/2000/svg"
-                                width="17"
-                                height="16"
-                                viewBox="0 0 17 16"
+                                width="14"
+                                height="14"
+                                viewBox="0 0 22 2"
                                 fill="none"
                                 className="cursor-pointer"
                               >
