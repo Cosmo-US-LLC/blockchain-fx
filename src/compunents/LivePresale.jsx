68d0364e4c770a58f1e40c98902d9c088f0e3e56
faheem303 <faheemrehmat360@gmail.com>
2025-08-26 18:05:25 +0500

    timer changes

--- src/compunents/LivePresale.jsx
+++ src/compunents/LivePresale.jsx
@@ -51,42 +51,14 @@ function LivePresale() {
 
   return (
     <div
-      className="flex items-center sm:flex-col flex-col 2xl:flex-row xl:flex-row lg:flex-row md:flex-row w-[100%] justify-center 2xl:mb-12 xl:mb-12 lg:mb-12 md:mb-12 sm:mb-0 mb-0 fixed 2xl:bottom-unset xl:bottom-unset lg:bottom-unset md:bottom-unset sm:bottom-0 bottom-0 2xl:top-0 2xl:h-[56px] xl:h-[56px] lg:h-[56px] md:h-[56px] sm:h-[98px] h-[98px] xl:top-0 lg:top-0 md:top-0 sm:top-unset top-unset left-0 right-0 z-[999]"
+      className="flex items-center sm:flex-col flex-col 2xl:flex-row 2xl:py-0 xl:py-0 lg:py-0 md:py-0 sm:py-3 py-3 xl:flex-row lg:flex-row md:flex-row w-[100%] justify-center 2xl:mb-12 xl:mb-12 lg:mb-12 md:mb-12 sm:mb-0 mb-0 fixed 2xl:bottom-unset xl:bottom-unset lg:bottom-unset md:bottom-unset sm:bottom-0 bottom-0 2xl:top-0 2xl:h-[56px] xl:h-[56px] lg:h-[56px] md:h-[56px] sm:h-[103px] h-[103px] xl:top-0 lg:top-0 md:top-0 sm:top-unset top-unset left-0 right-0 z-[999]"
       style={{
         background:
           "radial-gradient(50% 50% at 50% 50%, #FFEBAC 0%, #FCD149 100%)",
       }}
     >
-      <div className="2xl:hidden xl:hidden lg:hidden pb-2 md:hidden sm:flex flex space-x-2 text-center">
-        <div>
-          <p className="text-[14px] font-bold">
-            {timeLeft.days.toString().padStart(2, "0")}
-          </p>
-          <p className="text-[8px]">Days</p>
-        </div>
-        <p className="text-[14px] font-bold">:</p>
-        <div>
-          <p className="text-[14px] font-bold">
-            {timeLeft.hours.toString().padStart(2, "0")}
-          </p>
-          <p className="text-[8px]">Hours</p>
-        </div>
-        <p className="text-[14px] font-bold">:</p>
-        <div>
-          <p className="text-[14px] font-bold">
-            {timeLeft.minutes.toString().padStart(2, "0")}
-          </p>
-          <p className="text-[8px]">Minutes</p>
-        </div>
-        <p className="text-[14px] font-bold">:</p>
-        <div>
-          <p className="text-[14px] font-bold">
-            {timeLeft.seconds.toString().padStart(2, "0")}
-          </p>
-          <p className="text-[8px]">Seconds</p>
-        </div>
-      </div>
-      <div className="max-w-[1200px] mx-auto w-[100%] 2xl:pt-0 xl:pt-0 lg:pt-0 md:pt-0 sm:pt-2 pt-2 2xl:border-t-[0px] xl:border-t-[0px] lg:border-t-[0px] md:border-t-[0px] sm:border-t-[1px] border-t-[1px] border-[#000] flex flex-row 2xl:justify-between xl:justify-between lg:justify-between md:justify-between sm:justify-center justify-center  2xl:space-x-[30px] xl:space-x-[30px] lg:space-x-[30px] md:space-x-[30px] sm:space-x-[15px] space-x-[15px] items-center px-2">
+     
+      <div className="max-w-[1200px] mx-auto w-[100%] 2xl:pb-0 xl:pb-0 lg:pb-0 md:pb-0 sm:pb-3 pb-3 2xl:border-b-[0px] xl:border-b-[0px] lg:border-b-[0px] md:border-b-[0px] sm:border-b-[1px] border-b-[1px] border-[#000] flex flex-row 2xl:justify-between xl:justify-between lg:justify-between md:justify-between sm:justify-center justify-center  2xl:space-x-[30px] xl:space-x-[30px] lg:space-x-[30px] md:space-x-[30px] sm:space-x-[15px] space-x-[15px] items-center px-2">
         <p className="font-[700] 2xl:text-[12px] xl:text-[12px] lg:text-[12px] md:text-[12px] sm:text-[10px] text-[10px] leading-[110%]">
           {t("live_presale_section.title")}{" "}
           <span className="font-[600]">
@@ -124,11 +96,48 @@ function LivePresale() {
         </div>
         <button
           onClick={handleScroll}
-          className="2xl:text-[12px] xl:text-[12px] lg:text-[12px] md:text-[10px] sm:text-[9px] text-[8px] font-[700] bg-[#000] text-[#fff] rounded-[3px] h-[25px] px-[6px] md:px-[10px]"
+          className="2xl:text-[12px] 2xl:flex xl:flex lg:flex md:flex items-center justify-center sm:hidden hidden xl:text-[12px] lg:text-[12px] md:text-[10px] sm:text-[9px] text-[8px] font-[700] bg-[#000] text-[#fff] rounded-[3px] h-[25px] px-[6px] md:px-[10px]"
+        >
+          {t("live_presale_section.buy_button")}
+        </button>
+          <button
+          onClick={handleScroll}
+          className="2xl:text-[12px] 2xl:hidden xl:hidden lg:hidden md:hidden items-center justify-center sm:flex flex xl:text-[12px] lg:text-[12px] md:text-[10px] sm:text-[9px] text-[8px] font-[700] border-[1px] border-[#000] text-[#000] rounded-[3px] h-[25px] px-[6px] md:px-[10px]"
         >
           {t("live_presale_section.buy_button")}
         </button>
       </div>
+ <div className="2xl:hidden xl:hidden lg:hidden pt-[5px] md:hidden sm:flex flex space-x-2 text-center">
+        <div>
+          <p className="text-[14px] font-bold">
+            {timeLeft.days.toString().padStart(2, "0")}
+          </p>
+          <p className="text-[8px]">Days</p>
+        </div>
+        <p className="text-[14px] font-bold">:</p>
+        <div>
+          <p className="text-[14px] font-bold">
+            {timeLeft.hours.toString().padStart(2, "0")}
+          </p>
+          <p className="text-[8px]">Hours</p>
+        </div>
+        <p className="text-[14px] font-bold">:</p>
+        <div>
+          <p className="text-[14px] font-bold">
+            {timeLeft.minutes.toString().padStart(2, "0")}
+          </p>
+          <p className="text-[8px]">Minutes</p>
+        </div>
+        <p className="text-[14px] font-bold">:</p>
+        <div>
+          <p className="text-[14px] font-bold">
+            {timeLeft.seconds.toString().padStart(2, "0")}
+          </p>
+          <p className="text-[8px]">Seconds</p>
+        </div>
+      </div>
+
+      
     </div>
   );
 }
