7be80ea7e715e4bdeb02718db3275ae8974f7f54
faheem303 <faheemrehmat360@gmail.com>
2025-08-27 17:12:04 +0500

    banner updates mobile bg

--- src/compunents/LivePresale.jsx
+++ src/compunents/LivePresale.jsx
@@ -51,11 +51,7 @@ function LivePresale() {
 
   return (
     <div
-      className="2xl:flex xl:flex lg:flex md:block sm:block block 2xl:items-center xl:items-center lg:items-center md:items-center sm:items-start items-start sm:flex-col flex-col 2xl:flex-row 2xl:py-0 xl:py-0 lg:py-0 md:py-0 sm:py-3 py-3 xl:flex-row lg:flex-row md:flex-row w-[100%] justify-center 2xl:mb-12 xl:mb-12 lg:mb-12 md:mb-12 sm:mb-0 mb-0 fixed 2xl:bottom-unset xl:bottom-unset lg:bottom-unset md:bottom-unset sm:bottom-0 bottom-0 2xl:top-0 2xl:h-[48px] xl:h-[48px] lg:h-[48px] md:h-[48px] sm:h-[60px] h-[60px] xl:top-0 lg:top-0 md:top-0 sm:top-unset top-unset left-0 right-0 z-[999] "
-      style={{
-        background:
-          "radial-gradient(50% 50% at 50% 50%, #FFEBAC 0%, #FCD149 100%)",
-      }}
+      className="2xl:flex xl:flex lg:flex md:block sm:block block 2xl:items-center xl:items-center lg:items-center md:items-center sm:items-start items-start sm:flex-col flex-col 2xl:flex-row 2xl:py-0 xl:py-0 lg:py-0 md:py-0 sm:py-3 py-3 xl:flex-row lg:flex-row md:flex-row w-[100%] justify-center 2xl:mb-12 xl:mb-12 lg:mb-12 md:mb-12 sm:mb-0 mb-0 fixed 2xl:bottom-unset xl:bottom-unset lg:bottom-unset md:bottom-unset sm:bottom-0 bottom-0 2xl:top-0 2xl:h-[48px] xl:h-[48px] lg:h-[48px] md:h-[48px] sm:h-[60px] h-[60px] xl:top-0 lg:top-0 md:top-0 sm:top-unset top-unset left-0 right-0 z-[999] bg_banner"
     >
      <div className="2xl:hidden xl:hidden lg:hidden md:hidden sm:flex flex px-2 max-w-[350px] mx-auto">
        <p className="font-[700] text-[11px] leading-[110%] ">
