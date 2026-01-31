7eb33271a0e8a658033c1ef54a170b9a56d9c710
saiksaif <saifali9196@gmail.com>
2026-01-31 15:03:38 +0500

    header fix

--- src/compunents/LivePresale.jsx
+++ src/compunents/LivePresale.jsx
@@ -153,6 +153,37 @@ const handleScroll = () => {
      <div
       className="flex items-center w-[100%] 2xl:mb-12 xl:mb-12 lg:mb-12 md:mb-12 sm:mb-0 mb-0 fixed 2xl:bottom-unset xl:bottom-unset lg:bottom-unset md:bottom-unset sm:bottom-0 bottom-0 2xl:top-0 h-[38px] xl:top-0 lg:top-0 md:top-0 sm:top-unset top-unset left-0 right-0 z-[999]"
       style={{
+        background: "#FCD149",
+        // background: "radial-gradient(50% 50% at 50% 50%, #FFEBAC 0%, #FCD149 100%)",
+      }}
+    >
+      <div className="max-w-[900px] mx-auto w-[100%] flex flex-row justify-evenly 2xl:space-x-[30px] xl:space-x-[30px] lg:space-x-[30px] md:space-x-[30px] sm:space-x-[15px] space-x-[15px] items-center px-0">
+        <p className="font-[600] 2xl:text-[16px] xl:text-[16px] lg:text-[16px] md:text-[16px] sm:text-[11px] text-[11px]">
+          {/* {t("live_presale_section.title")} */}
+          The <strong>BFX App</strong> is now live in 100+ countries at <strong>BlockFX.com</strong>
+        </p>
+        {/* <img
+          src={live_cards}
+          alt="live_cards"
+          className="block 2xl:hidden xl:hidden lg:hidden md:hidden sm:block"
+        />
+        <img
+          src={live_cards_des}
+          alt="live_cards"
+          className="hidden 2xl:block xl:block lg:block md:block sm:hidden"
+        /> */}
+        <button
+          onClick={handleScroll}
+          className="2xl:text-[12px] xl:text-[12px] lg:text-[12px] md:text-[10px] sm:text-[9px] text-[8px] font-[700] bg-[black] text-white rounded-[3px] h-[25px] px-[6px] md:px-[10px]"
+        >
+          {t("live_presale_section.buy_button")}
+        </button>
+      </div>
+    </div>
+
+     {/* <div
+      className="flex items-center w-[100%] 2xl:mb-12 xl:mb-12 lg:mb-12 md:mb-12 sm:mb-0 mb-0 fixed 2xl:bottom-unset xl:bottom-unset lg:bottom-unset md:bottom-unset sm:bottom-0 bottom-0 2xl:top-0 h-[38px] xl:top-0 lg:top-0 md:top-0 sm:top-unset top-unset left-0 right-0 z-[999]"
+      style={{
         background: "radial-gradient(50% 50% at 50% 50%, #FFEBAC 0%, #FCD149 100%)",
       }}
     >
@@ -177,7 +208,7 @@ const handleScroll = () => {
           {t("live_presale_section.buy_button")}
         </button>
       </div>
-    </div>
+    </div> */}
    
     </>
   );
