e991b7f497e0158758b60945c15eb3774d42032a
jahanzaib-cosmoinc <jahanzaibbaig@cosmoinc.co>
2026-09-04 12:53:53 +0500

    Image updated

--- src/Home/Herosection.jsx
+++ src/Home/Herosection.jsx
@@ -135,7 +135,7 @@ function Herosection() {
               ></wistia-player>
               */}
               <img
-                src="assets/blockchainfx/HeroSection/BFX-AllTimeHigh.webp"
+                src="assets/blockchainfx/HeroSection/new-BFX-AllTimeHigh.webp"
                 alt="BlockchainFX All Time High"
                 style={{
                   width: "100%",
